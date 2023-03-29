dde33e8ea50ff50c93f52a865a71fa8b145ea6f6
GuilhermeLCS <guilhermeluiscdasilva@hotmail.com>
2023-03-29 00:00:41 -0300

    Ready

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,31 +5,6 @@
 
 // LinkedIn,Github and CV icons
 
-function newImgLinkedin() {
-    document.getElementById("img1").src = "js/icons/linkedin.png";
-}
-function oldImgLinkedin() {
-    document.getElementById("img1").src = "js/icons/linkedingray.png";
-}
-function newImgGithub() {
-    document.getElementById("img2").src = "js/icons/github.png";
-}
-function oldImgGithub() {
-    document.getElementById("img2").src = "js/icons/githubgray.png";
-}
-function newImgResume() {
-    document.getElementById("img3").src = "js/icons/resumeicon.png";
-}
-function oldImgResume() {
-    document.getElementById("img3").src = "js/icons/resumeicongray.png";
-}
-
-function newImgResume() {
-    document.getElementById("img3").src = "js/icons/resumeicon.png";
-}
-function oldImgResume() {
-    document.getElementById("img3").src = "js/icons/resumeicongray.png";
-}
 
 // Icons in the cards
 
